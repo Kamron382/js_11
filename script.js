11bfe0c628abbaee7e041ee6c61eb72cf661ce27
Kamron <k1367259@gmail.com>
2025-05-31 13:22:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let text = document.querySelector('p')
+let body = document.querySelector('body')
+let n = prompt('Write text color')
+let background = prompt('Write body color')
+text.style.color = n
+body.style.background = background
